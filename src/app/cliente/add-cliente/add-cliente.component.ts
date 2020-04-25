97f0f4116c013c350f2cfb3bb7e013877e9c104c
Geison Correia <geisonthcorreia@gmail.com>
2020-04-25 19:01:30 -0300

    Implementar cadastro de novo cliente

--- src/app/cliente/add-cliente/add-cliente.component.ts
+++ src/app/cliente/add-cliente/add-cliente.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ClienteService } from '../cliente.service';
+import { Router } from '@angular/router';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-add-cliente',
@@ -7,9 +20,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddClienteComponent implements OnInit {
 
-  constructor() { }
+  clienteForm: FormGroup;
+  nome = '';
+  idade: number = null;
+  email = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+
+  constructor(private router: Router, private api: ClienteService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.clienteForm = this.formBuilder.group({
+      nome : [null, Validators.required],
+      idade : [null, Validators.required],
+      email : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCliente(this.clienteForm.value)
+      .subscribe((res: any) => {
+          const id = res.id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/cliente-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
   }
 
 }
